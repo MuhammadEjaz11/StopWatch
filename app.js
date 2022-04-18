402af3c67d4ff1f4b9ceb405a1b010f443b6eb84
Muhammad Ejaz <98952856+MuhammadEjaz11@users.noreply.github.com>
2022-04-18 05:49:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+var hour = 0;
+var minute = 0;
+var sec = 0;
+
+var interval ; 
+
+
+
+function stop() {
+    clearInterval(interval )
+    
+}
+
+function reset() {
+    clearInterval(interval )
+
+    
+    hour = 0;
+    minute = 0;
+    sec = 0;
+
+    document.getElementById("hour").innerHTML = "00";
+    document.getElementById("minute").innerHTML = "00";
+    document.getElementById("sec").innerHTML = "00";
+}
+
+function stopWatch() {
+
+
+    sec = ++sec;
+
+    document.getElementById("sec").innerHTML =  "0"+ sec ;
+    if(sec>=10){
+
+    document.getElementById("sec").innerHTML = sec ;
+
+    }
+    if (sec == 60) {
+        minute = ++minute;
+        sec = 0;
+        document.getElementById("minute").innerHTML = minute;
+    }
+    if (minute == 60) {
+        document.getElementById("hour").innerHTML = hour;
+        hour = ++hour;
+        minute = 0;
+        sec = 0;
+
+
+    }
+
+
+
+
+}
+function start() {
+   
+    interval =  setInterval(stopWatch, 1000)
+
+
+
+}
